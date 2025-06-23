f6a43d4202384418fd7bdb859d531efd1585d839
rudrathakur14 <anveshsingh0612@gmail.com>
2025-06-23 16:09:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -241,6 +241,7 @@ def upload():
         return "Internal Server Error", 500
 
 # --- Run App ---
+import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
